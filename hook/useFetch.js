1252d81c6e6894b226f706beff45edcb96c93499
alifzwan <alifzakwan529@gmail.com>
2024-03-05 15:12:44 +0800

    made the Fetch file to fetch the info from the Rapid API

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,47 +1,56 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const useFetch = (endpoint, query) => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const options = {
-    method: "GET",
+    method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
-      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+      'X-RapidAPI-Key': "f773a2fc92mshc51eb769aa26e93p18ebbejsnc9e93e84baea",
+      'X-RapidAPI-Host':'jsearch.p.rapidapi.com'
     },
-    params: { ...query },
-  };
-
-  const fetchData = async () => {
-    setIsLoading(true);
-
-    try {
-      const response = await axios.request(options);
-
-      setData(response.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setError(error);
-      console.log(error)
-    } finally {
-      setIsLoading(false);
+    params: {
+      ...query
     }
   };
+  
 
+  const fetchData = async () => {
+
+    setIsLoading(true)
+
+    try {
+
+      const response = await axios.request(options);
+
+      setData(response.data.data)
+      setIsLoading(false)
+
+    } catch(error){
+
+      setError(error)
+      alert("Something went wrong");
+
+    } finally{
+      setIsLoading(false)
+    }
+  }
+  
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   const refetch = () => {
-    setIsLoading(true);
-    fetchData();
-  };
+    setIsLoading(true)
+    fetchData()
+  }
 
-  return { data, isLoading, error, refetch };
-};
+  return { data, isLoading, error, refetch }
+}
 
-export default useFetch;
+export default useFetch
