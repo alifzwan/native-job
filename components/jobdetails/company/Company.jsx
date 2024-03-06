18fd903bf08218959efced710452c11947fbbca5
alifzwan <alifzakwan529@gmail.com>
2024-03-06 16:55:19 +0800

    tidy up

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -8,32 +8,34 @@ import { checkImageURL } from "../../../utils";
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.logoBox}>
-        <Image
-          source={{
-            uri: checkImageURL(companyLogo)
+       <View style={styles.logoBox}>
+          <Image 
+            source={{
+              uri:checkImageURL(companyLogo)
               ? companyLogo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
-          }}
-          style={styles.logoImage}
-        />
-      </View>
-
-      <View style={styles.jobTitleBox}>
-        <Text style={styles.jobTitle}>{jobTitle}</Text>
-      </View>
-
-      <View style={styles.companyInfoBox}>
-        <Text style={styles.companyName}>{companyName} / </Text>
-        <View style={styles.locationBox}>
-          <Image
-            source={icons.location}
-            resizeMode='contain'
-            style={styles.locationImage}
+            }} 
+            style={styles.logoImage}
           />
-          <Text style={styles.locationName}>{location}</Text>
-        </View>
-      </View>
+       </View>
+
+       <View style={styles.jobTitleBox}>
+          <Text style={styles.jobTitle}>{jobTitle}</Text>
+
+          <View style={styles.companyInfoBox}>
+            <Text style={styles.companyName}>{companyName}</Text>
+            <View style={styles.locationBox}>
+              <Image 
+                source={icons.location} 
+                style={styles.locationImage}
+                resizeMode="contain"
+              />
+              <Text style={styles.locationName}>{location}</Text>
+            </View>
+          </View>
+       </View>
+
+        
     </View>
   );
 };
