5cedfcbbc62b2f7c98cac16dd8f45a298069740e
alifzwan <alifzakwan529@gmail.com>
2024-03-07 10:16:20 +0800

    added subpages on the [slug] and the button that classified that subpages

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -1,11 +1,19 @@
 import { View, Text } from "react-native";
-
 import styles from "./specifics.style";
 
-const Specifics = () => {
+const Specifics = ({title, details}) => {
   return (
-    <View>
-      <Text>Specifics</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title} :</Text>
+
+      <View style={styles.pointsContainer}>
+        {details.map((item) => (
+          <View key={item} style={styles.pointWrapper}>
+            <View style={styles.pointDot}/>
+            <Text style={styles.pointText}>{item}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
