09e09341251da436afe9558668612f6fc54a96ac
alifzwan <alifzakwan529@gmail.com>
2024-03-07 10:15:17 +0800

    added footer with like and apply button on the [slug] page of PopularJobs

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -7,18 +7,14 @@ const Footer = ({ url }) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.likeBtn}>
-        <Image
-          source={icons.heartOutline}
-          resizeMode='contain'
-          style={styles.likeBtnImage}
-        />
+        <Image source={icons.heart} style={styles.likeBtnImage}/>
       </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.applyBtn}
-        onPress={() => Linking.openURL(url)}
-      >
-        <Text style={styles.applyBtnText}>Apply for job</Text>
+      <TouchableOpacity 
+        style={styles.applyBtn} 
+        onPress={() => Linking.openURL(url)}>
+          
+        <Text style={styles.applyBtnText}>Apply For Jobs</Text>
       </TouchableOpacity>
     </View>
   );
